7f28f8af82412e98333c75ff64e84d63994e9ee2
Andreisys <andrei.selaru96@gmail.com>
2025-03-04 12:41:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![Superseed](logo.png)
+
+# Superseed node
+
+Superseed is an Ethereum Layer 2 that rewards those who DeFi. It's built on Optimism's open-source [OP Stack](https://stack.optimism.io/).
+
+This repository contains the relevant Docker builds to run your own node on the Superseed network.
---
+++ scripts/superseed-mainnet/start-op-node.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Start op-node.
+exec op-node \
+    --l1=https://your-eth-${NETWORK_NAME}-rpc-endpoint \
+    --l2=ws://op-geth:8551 \
+    --rpc.addr=0.0.0.0 \
+    --rpc.port=9545 \
+    --l2.jwt-secret=/op-data/secret.jwt \
+    --metrics.enabled \
+    --rollup.load-protocol-versions=true \
+    --rollup.config /config/${NETWORK_NAME}/rollup.json \
+    --override.holocene=1736445601 \
+    --override.granite=1726185601 \
+    --l1.beacon=https://your-beacon-node-endpoint \
+    --p2p.bootnodes=enode://your-bootnodes \
+    --p2p.static=/ip4/static-ip
